f137a4642281dacba3f6376a6b87c1fcb54c312f
Brian <tzhlee@gmail.com>
2017-04-21 00:14:40 -0500

    Coundown implemented

--- public/app.jsx
+++ public/app.jsx
@@ -1,7 +1,23 @@
 var LightningCounter = React.createClass({
+    getInitialState: function () {
+        return {
+            strikes: 0
+        };
+    },
+
+    timerTick: function(){
+        this.setState({
+            strikes: this.state.strikes + 100
+        });
+    },
+
+    componentDidMount: function(){
+        setInterval(this.timerTick, 1000);
+    },
+
     render: function() {
         return(
-            <h1>Hello!</h1>
+            <h1>{this.state.strikes}</h1>
         );
     }
 });
