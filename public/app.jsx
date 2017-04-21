6de0eecf2ea64b2b4aa3814ceb46d9d3f82f8360
Brian <tzhlee@gmail.com>
2017-04-21 13:33:11 -0500

    CSS interace completed

--- public/app.jsx
+++ public/app.jsx
@@ -16,14 +16,23 @@ var LightningCounter = React.createClass({
     },
 
     render: function() {
+        var counterStyle = {
+            color: "#66FFFF",
+            fontsize: 50
+        };
+        var count = this.state.strikes.toLocaleString()
         return(
-            <h1>{this.state.strikes}</h1>
+            <h1 style={counterStyle}>{count}</h1>
         );
     }
 });
 
 var LightningCounterDisplay = React.createClass({
     render: function() {
+        var commonStyle = {
+            margin: 0,
+            padding: 0
+        } 
         var divStyle = {
             width: 250,
             textAlign: "center",
@@ -34,9 +43,28 @@ var LightningCounterDisplay = React.createClass({
             borderRadius: 10
         };
 
+        var textStyles = {
+            emphasis: {
+                fontSize: 38,
+                ...commonStyle
+            },
+            smallEmphasis: {
+                ...commonStyle
+            },
+            small: {
+                fontSize: 17,
+                opacity: 0.5,
+                ...commonStyle
+            }
+        
+        }
+
         return(
             <div style={divStyle}>
                 <LightningCounter/>
+                <h2 style={textStyles.smallEmphasis}>LIGHTNING STRIKES</h2>
+                <h2 style={textStyles.emphasis}>WORLDWIDE</h2>
+                <p style={textStyles.small}>(since you loaded this example)</p>
             </div>
         );
     }
